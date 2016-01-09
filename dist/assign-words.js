63fabd97fd3c5852cc3cebefb3022fddb743d1eb
sohamkamani <sohamkamani@gmail.com>
2016-01-09 19:41:26 +0530

    fixed new line bug

--- dist/assign-words.js
+++ dist/assign-words.js
@@ -19,10 +19,15 @@ module.exports = function (fileString, language) {
   var colorString = '';
   tokens.forEach(function (token) {
     var color = (0, _scopesToColor2.default)(token.scopes);
-    colorString += _chalk2.default[color](token.value);
+    var colouredToken = token.value.split('\n').map(function (val) {
+      return _chalk2.default[color](val);
+    }).join(_chalk2.default[color]('\n'));
+    console.log('log:', token.value, colouredToken);
+    colorString += colouredToken;
   });
 
   //Adding line numbers
+  // console.log('log:',tokens);
   var colorStringLineNos = colorString.split('\n').map(function (eachLine, n) {
     return _chalk2.default.bgWhite.black(' ' + (n + 1) + ' \t') + eachLine;
   }).join('\n');
--- source/assign-words.js
+++ source/assign-words.js
@@ -7,11 +7,14 @@ module.exports = function (fileString, language) {
   let tokens = encodeLang(fileString, language);
   let colorString = '';
   tokens.forEach(token => {
-    var color = scopesToColor(token.scopes);
-    colorString += chalk[color](token.value);
+    const color = scopesToColor(token.scopes);
+    let colouredToken = token.value.split('\n').map(val => chalk[color](val)).join(chalk[color]('\n'));
+    // console.log('log:',token.value, colouredToken);
+    colorString += colouredToken;
   });
 
   //Adding line numbers
+  // console.log('log:',tokens);
   let colorStringLineNos = colorString.split('\n').map( (eachLine, n) => {
     return chalk.bgWhite.black(` ${n + 1} \t`) + eachLine;
   }).join('\n');
